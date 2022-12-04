a09d7ddcac6ddeeb2f00f0c6fb550c78e23e14b9
Hieyson <77486405@continental.edu.pe>
2022-12-04 16:16:37 -0500

    Actualizacion de la gestion de la Asignatura

--- src/logica/gestionAsignatura.py
+++ src/logica/gestionAsignatura.py
@@ -6,26 +6,27 @@ class GestionAsignatura():
     def __init__(self):
         Base.metadata.create_all(engine)
 
-    def agregar_asignatura(self, nombreAsignatura):
+    def agregar_asignatura(self, nombreAsignatura, Creditos):
         if len(nombreAsignatura)==0:
             return False
         busqueda = session.query(Asignatura).filter(Asignatura.nombreAsignatura == nombreAsignatura).all()
         if len(busqueda) == 0:
-            asignatura = Asignatura(nombreAsignatura=nombreAsignatura)
+            asignatura = Asignatura(nombreAsignatura=nombreAsignatura, credito = Creditos)
             session.add(asignatura)
             session.commit()
             return True
         else:
             return False
 
-        def editar_asignatura(self, idAsignatura, nombreAsignatura):
-            if len(nombreAsignatura) == 0:
-                return False
-            busqueda = session.query(Asignatura).filter(Asignatura.nombreAsignatura == nombreAsignatura).all()
-            if len(busqueda) == 0:
-                asignatura = session.query(Asignatura).filter(Asignatura.idAsignatura == idAsignatura).first()
-                asignatura.nombreAsignatura = nombreAsignatura
-                session.commit()
-                return True
-            else:
-                return False
+    def editar_asignatura(self, idAsignatura, nombreAsignatura, Creditos):
+        if len(nombreAsignatura) == 0:
+           return False
+        busqueda = session.query(Asignatura).filter(Asignatura.nombreAsignatura == nombreAsignatura).all()
+        if len(busqueda) == 0:
+            Asignatura = session.query(Asignatura).filter(Asignatura.idAsignatura == idAsignatura).first()
+            Asignatura.nombreAsignatura = nombreAsignatura
+            Asignatura.creditos = Creditos
+            session.commit()
+            return True
+        else:
+            return False
